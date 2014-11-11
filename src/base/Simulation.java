61097a85151b6084f9031643e51fcf309752c5eb
Wade Ashby <mashby6@gatech.edu>
2014-11-10 23:35:48 -0600

    Started updating temps in Simulation. Added EarthModel package to look at from Project 2.

--- src/base/Simulation.java
+++ src/base/Simulation.java
@@ -1,5 +1,24 @@
 package base;
 
-public class Simulation {
+public class Simulation implements SimulationMethod{
+	private static final double TEMPERATURE_INCREMENT = 5;
 
+	@Override
+	public SimulationResult simulate(SimulationResult previousResult, double axialTilt, double orbitalEccentricity, int sunPosition) throws InterruptedException {
+		int columnCount = previousResult.getColumnCount();
+		int rowCount = previousResult.getRowCount();
+		
+		double[][] newPlanet = new double[rowCount][];
+		for (int i = 0; i < newPlanet.length; i++) {
+			double[] currentRow = new double[columnCount];
+			for (int j = 0; j < currentRow.length; j++) {
+				currentRow[j] = previousResult.getTemperature(i, j) + TEMPERATURE_INCREMENT + i * 5;
+			}
+			newPlanet[i] = currentRow;
+		}
+		
+		Thread.sleep(500);
+		
+		return new SimulationResult(newPlanet);
+	}
 }
