4cabdc5251ee4429dc0dec198ba6043e78eac686
Wade Ashby <mashby6@gatech.edu>
2014-11-16 21:00:17 -0600

    Fixed one method that had not been updated for disabling presentation thread.

--- src/controllers/MasterController.java
+++ src/controllers/MasterController.java
@@ -26,7 +26,7 @@ public class MasterController {
 	
 	public void pause() throws ThreadException {
 		mSimulationController.pause();
-		
+		if(this.runPresentation)
 			mPresenationController.pause();
 	}
 	
