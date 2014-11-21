c1367f00774f59c1b8ac3cbcb7c33b2ff680bc5a
Aaron Higdon <ahigdon@chegg.com>
2014-11-21 15:47:36 -0500

    Added test for thread local storage of db connection

--- test/data/impl/JavaDBDatastoreTest.java
+++ test/data/impl/JavaDBDatastoreTest.java
@@ -1,9 +1,12 @@
 package data.impl;
 
+import java.sql.SQLException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import base.ObjectFactory;
 import data.Datastore;
 
 public class JavaDBDatastoreTest {
@@ -20,5 +23,37 @@ public class JavaDBDatastoreTest {
 	public void testReset() {
 		Datastore ds = JavaDBDatastore.getInstance();
 		ds.reset();
+	}
+	
+	@Test
+	public void testMultipleThreads(){
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 100; i++) {
+					ObjectFactory.getSimulationDAO().getSimulationNames();
+					if(i%10 == 0 && "TestThread-14".equals(Thread.currentThread().getName()) ){
+						try {
+							System.out.println(Thread.currentThread().getName() + " - Closed Connection");
+							JavaDBDatastore.getInstance().getConnection().close();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+				System.out.println(Thread.currentThread().getName() + " - COMPLETE");
+			}
+		};
+		
+		for (int i = 0; i < 25; i++) {
+			Thread t = new Thread(r, "TestThread-"+i);
+			t.start();
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
