3d1b1c5705de7fced92c40e59ff38e23eee17026
Aaron Higdon <ahigdon@chegg.com>
2014-11-26 00:13:20 -0500

    Saved the simulation grid size to use when retrieving grid in query

--- src/data/impl/SimulationResultDAOConstants.java
+++ src/data/impl/SimulationResultDAOConstants.java
@@ -6,6 +6,8 @@ public interface SimulationResultDAOConstants {
 	public static final String ID               = "ID";
 	public static final String SIMULATION_ID    = "SIMULATION_ID";
 	public static final String SIMULATION_TIME  = "SIMULATION_TIME";
+	public static final String ROW_COUNT        = "ROW_COUNT";
+	public static final String COLUMN_COUNT     = "COLUMN_COUNT";
 	public static final String OCCURENCE_DATE   = "OCCURENCE_DATE";
 	public static final String OCCURENCE_TIME   = "OCCURENCE_TIME";
 	public static final String SUN_LONGITUDE    = "SUN_LONGITUDE";
