366d8f4f69cf1985562df8076c371854988d0ca8
Tyler Benfield <rtbenfield@gmail.com>
2014-11-29 23:11:14 -0500

    Removed EarthModel code

--- src/EarthModel/Sun.java
+++
@@ -1,9 +0,0 @@
-/*
- * This code came from Wade Ashby's teams Project 2 code
- */
-package EarthModel;
-
-public class Sun {
-	public static final double TOTALENERGY = 1740 * Math.pow(10, 11); //KW
-	public static final double HPSKM = 0.001; //KW
-}
